8a08b3ddc9e6eea7d7c31b90467300d5395689d3
Nahu Caroseli <nahucaroseli2100@gmail.com>
2024-12-11 15:11:48 -0300

    modificado orden del codigo

--- server.js
+++ server.js
@@ -4,12 +4,18 @@ const mongoose = require('mongoose');
 
 const dotenv = require('dotenv');
 const cors = require("cors");
-const authRoutes = require('./routes/auth');
+
 
 dotenv.config();
 
 
 const app = express();
+
+mongoose.connect(process.env.MONGO_URI,{useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log("Conectado"))
+.catch((error)=> console.log("No conectado",error));
+
+
 
 app.use(cors());
 app.use(express.json());
@@ -17,15 +23,12 @@ app.use(express.json());
 
 
 
-
+const authRoutes = require('./routes/auth');
 
 app.use('/api/auth',authRoutes);
 
 console.log(process.env.MONGO_URI);
 
-mongoose.connect(process.env.MONGO_URI,{useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => console.log("Conectado"))
-.catch((error)=> console.log("No conectado",error));
 
 
 
