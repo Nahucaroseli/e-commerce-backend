933bc73ace465cb65216e3cd05b53b345bc18ca3
Nahu Caroseli <nahucaroseli2100@gmail.com>
2025-06-05 14:19:57 -0300

    agregado parametro faltante para cors

--- server.js
+++ server.js
@@ -18,7 +18,8 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.use(cors({
   origin: "https://compra-facil.netlify.app",
-  methods: ["GET", "POST"]
+  methods: ["GET", "POST"],
+  credentials: true
 }));
 
 
