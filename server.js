bca794e0e6bdc4b65367a1faa973b19cf3f68df0
Nahu Caroseli <nahucaroseli2100@gmail.com>
2025-06-05 13:24:55 -0300

    modificado mensaje de error y URI de mongoose

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 
 mongoose.connect(process.env.MONGO_URI,{useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => console.log("Conectado"))
-.catch((error)=> console.log("No conectado",error));
+.catch((error)=> console.log("No se ha conectado, ocurrio un problema",error));
 
 
 
